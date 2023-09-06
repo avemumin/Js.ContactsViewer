a01fc2612b50a36bdd7704cb96f65fa4b43119fc
Jacek Szczepański <jacogitproject@gmail.com>
2023-09-06 19:30:23 +0200

    Add project files.

---
+++ Js.ContactsViewer/Shared/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Js.ContactsViewer.Shared.Models
+{
+    internal class Contact
+    {
+    }
+}
